7e328f9178b16ab1d27a74cc3fd7df1be6a9e33b
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 18:28:16 -0300

    Fix variables TF Network

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "aks" {
   name     = var.resource_group_name
   location = var.location
 }
