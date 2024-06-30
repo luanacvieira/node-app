869948b796e1f5b9ef44a3752b2aca09e60af463
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 17:37:22 -0300

    Fix network TF

--- terraform/aks/network.tf
+++ terraform/aks/network.tf
@@ -1,20 +1,20 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myAKSVNet"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "myAKSSubnet"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = [var.subnet_address_prefix]
 }
 
 resource "azurerm_subnet" "subnet_node_pool" {
-  name                 = "myAKSNodePoolSubnet"
+  name                 = var.node_pool_subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = [var.node_pool_subnet_address_prefix]
 }
