63c9449eb4a7a6b1e093bffd53d031635c1bba09
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 18:18:25 -0300

    Fix variables TF Network TF

---
+++ terraform/aks/network.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
+
+resource "azurerm_subnet" "subnet_node_pool" {
+  name                 = var.node_pool_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.node_pool_subnet_address_prefix]
+}
