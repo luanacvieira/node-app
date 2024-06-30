23633dca2a1b092a71f56c907778813b5638a82e
luanacvieira <luanacvieira@users.noreply.github.com>
2024-06-30 04:11:18 -0300

    add network

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -19,6 +19,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = "Standard_DS2_v2"
   }
 
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.0.10.10"
+    service_cidr   = "10.0.10.0/24"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+  
   identity {
     type = "SystemAssigned"
   }
